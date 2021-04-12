8020b1e797ae1a8b6f710836d14666b076cb09da
Vishal Raj <vishalraj82@gmail.com>
2021-04-12 04:52:57 +0400

    Handlex extra props

--- src/Components/FlexBox/Box.js
+++ src/Components/FlexBox/Box.js
@@ -4,13 +4,13 @@ import cx from 'clsx';
 import styles from './FlexBox.scss';
 
 const Box = (props) => {
-    const { alignItems, justifyContent, className, children } = props;
+    const { alignItems, justifyContent, className, children, ...restProps } = props;
 
     const AlignItemsClassname = styles[`align-items-${alignItems}`];
     const JustifyClassname = styles[`justify-content-${justifyContent}`];
 
     return (
-        <div className={cx(styles.box, AlignItemsClassname, JustifyClassname, className)}>
+        <div className={cx(styles.box, AlignItemsClassname, JustifyClassname, className)} {...restProps}>
             {children}
         </div>
     );
